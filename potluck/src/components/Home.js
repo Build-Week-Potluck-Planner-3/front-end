95004d486d485fc7644ee9f5b4e10f386ac061dd
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-08-24 12:54:56 -0600

    added two links to home page with minor styling

--- potluck/src/components/Home.js
+++ potluck/src/components/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div>
             {/* Central hub for webpage and navigation*/}
+            <h1 className='home-heading'>Welcome to your Potluck Planner</h1>
+            <div className='home-card-container'>
+                <div className='home-card'>
+                    <Link to='/addPotluck'>
+                        <img className='home-card-img' src='https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8Zm9vZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60'></img>
+                        <p>Create A New Potluck</p>
+                    </Link>
+
+                </div>
+                <div className='home-card'>
+                    <Link to='/potlucks'>
+                        <img className='home-card-img' src='https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1650&q=80'></img>
+                        <p>View Existing Potlucks</p>
+                    </Link>
+
+                </div>
+
+            </div>
         </div>
     )
 }
