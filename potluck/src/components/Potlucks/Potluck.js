8e6b09ad92e8fcbee0ca95a20170c14c00238bf3
Jamaal Montasser <jamaal@Jamaals-Mini.attlocal.net>
2021-08-24 20:14:30 -0700

    potluck form with localized state and potluck component

--- potluck/src/components/Potlucks/Potluck.js
+++ potluck/src/components/Potlucks/Potluck.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 function Potluck(props) {
+    const {details} = props
     return (
-        <div>
+        <div className='potluck container'>
             {/* Presentational component to display prop data */}
+            <h2>{details.name}</h2>
+            <p>Date: {details.date}</p>
+            <p>Time: {details.time}</p>
+            <p>Location: {details.location}</p>
+            <p>Dishes: </p>
+            {dishes.forEach(element => {
+              return (
+              <p>{element.trim()}</p>)
+            })}
         </div>
     )
 }
